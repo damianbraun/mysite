c013b0b92d624655e4298d4fa343bb48c876b93e
Damian Braun <brunek5252@gmail.com>
2017-01-31 18:34:25 +0100

    class PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_list(request):
     object_list = Post.published.all()
@@ -13,9 +21,9 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request,
-                      'blog/post/list.html',
-                      {'page': page,
-                       'posts':posts})
+                  'blog/post/list.html',
+                  {'page': page,
+                  'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
